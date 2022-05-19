088acc7f6728e80f0099f09bf42255d9f5ea993e
yangyu <734880084@qq.com>
2022-05-19 19:50:08 +0800

    feat: 完成windicss配置

--- packages/editor/src/App.svelte
+++ packages/editor/src/App.svelte
@@ -3,18 +3,9 @@
 </script>
 
 <main>
-    <Counter />
+    <div class="bg-red-100 w-5/10 h-px-100"></div>
+    <Counter/>
 </main>
 
-<style>
-    :root {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-            Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-
-    main {
-        text-align: center;
-        padding: 1em;
-        margin: 0 auto;
-    }
+<style windi:global>
 </style>
--- packages/editor/windi.config.ts
+++ packages/editor/windi.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'windicss/helpers';
-import formsPlugin from 'windicss/plugin/forms';
+// import formsPlugin from 'windicss/plugin/forms';
 
 export default defineConfig({
     darkMode: 'class',
-    safelist: 'p-3 p-4 p-5',
+    // safelist: 'p-3 p-4 p-5',
     theme: {
         extend: {
             colors: {
@@ -13,5 +13,5 @@ export default defineConfig({
             },
         },
     },
-    plugins: [formsPlugin],
+    // plugins: [formsPlugin],
 });
