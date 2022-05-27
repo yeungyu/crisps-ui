6ff5bed1a4518f77296f6686b1ad8c5d96923184
yangyu <734880084@qq.com>
2022-05-27 21:01:09 +0800

    chore: "strictNullChecks": true

--- packages/editor/src/App.svelte
+++ packages/editor/src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import init from './test';
+    import init from './editorMain';
 
     onMount(() => {
         init();
--- packages/editor/src/main.ts
+++ packages/editor/src/main.ts
@@ -1,6 +1,5 @@
 import App from './App.svelte';
 import 'virtual:windi.css';
-import './test';
 
 const app = new App({
     target: document.getElementById('app'),
